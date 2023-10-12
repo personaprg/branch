27dcd4aaffb07fc677a450aca80cbe19f0ec33c7
SDH <personarpg7501@gmail.com>
2023-10-12 10:44:24 +0900

    git에 올라갔는지 재확인

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const http=reqire('http');
+
+const server = http.createServer((req, res)=>{
+
+});
+
+server.listen(3500, (err) => {
+    if(!err) {
+        console.error('서버 안돌아감')
+    }else{
+        console.log('3500 포트에 들어감 http://localhost:3500'); //꿀팁 이거 만들어두면 콘솔에서 그냥 클릭하면 된다고함!
+    }
+})
