a11c0a7c077ae5ae83a2f6211fad688b0be9b9a4
SDH <personarpg7501@gmail.com>
2023-10-12 10:57:30 +0900

    향마루 응답처리

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const http=reqire('http');
-
+const http = require('http');
 const server = http.createServer((req, res)=>{
-
+    res.writeHead(200, {"Content-Type": "text/plain"}); //html이 아니고 텍스트만 던져준다고 함!('plain'이 설명한다 라는 의미)
+    //이렇게 적으면 텍스트 보내겠다는 의미라고 함
+    res.write("향마루"); //쓴 텍스트
+    res.end();
+    //서버를 요청할때마다 "향마루가 출력 될 것"
 });
 
-server.listen(3500, (err) => {
-    if(!err) {
-        console.error('서버 안돌아감')
-    }else{
-        console.log('3500 포트에 들어감 http://localhost:3500'); //꿀팁 이거 만들어두면 콘솔에서 그냥 클릭하면 된다고함!
-    }
-})
+server.listen(3500);
